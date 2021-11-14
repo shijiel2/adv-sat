9a61e240ba744ada80d78716569b3909d215273c
Shijie Liu <shijiel2@student.unimelb.edu.au>
2021-11-14 13:17:54 +1100

    update main

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import socket
 from subprocess import call
 from notification import NOTIFIER
 
-CIFAR10_TRAIN_COMMAND = "python train.py --dataset cifar10 -n 60 --epoch_adv 0 --gpu 0 --arch wide --sgd --sink --sink_eps 1 -f wide_resnet_sink_cifar10.ckpt -c _wide_resnet_sink_cifar10"
+CIFAR10_TRAIN_COMMAND = "python train.py --dataset cifar10 -n 100 --epoch_adv 0 --gpu 0 --arch wide --sgd --sink --sink_eps 1 -f wide_resnet_sink_cifar10.ckpt -c _wide_resnet_sink_cifar10 --resume"
 CIFAR10_TEST_COMMAND = "python test_adv.py --arch wide --dataset cifar10 --eps 8 -a PGD -f wide_resnet_sink_cifar10.ckpt -l 8 --gpu 0"
 
-cmds = [CIFAR10_TRAIN_COMMAND, CIFAR10_TEST_COMMAND]
+CIFAR100_TRAIN_COMMAND = "python train.py --dataset cifar100 -n 100 --epoch_adv 0 --gpu 0 --arch wide --sgd --sink --sink_eps 1 -f wide_resnet_sink_cifar100.ckpt -c _wide_resnet_sink_cifar100 --resume"
+CIFAR100_TEST_COMMAND = "python test_adv.py --arch wide --dataset cifar100 --eps 8 -a PGD -f wide_resnet_sink_cifar100.ckpt -l 8 --gpu 0"
+
+cmds = [CIFAR100_TRAIN_COMMAND, CIFAR100_TEST_COMMAND]
 for cmd in cmds:
     proc = call(cmd.split())
 
